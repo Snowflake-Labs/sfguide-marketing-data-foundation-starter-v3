f3b19a88e185b812457c301359c8aec0eb2099ce
Joviane Bellegarde <joviane.bellegarde@snowflake.com>
2025-07-13 17:35:29 -0400

    updated session tagging for native app

--- scripts/backend/utils/model_helpers.py
+++ scripts/backend/utils/model_helpers.py
@@ -7,4 +7,11 @@ def execute_sql_query(_sp_session, query):
     return result.collect()  
 
 def connect_to_snowflake() -> Session:
-    return session()
+    sp_session = session()
+    sp_session.query_tag = {
+        "origin": "sf_sit-is", 
+        "name": "marketing_data_foundation_starter_spcs", 
+        "version": {"major": 3, "minor": 0},
+        "attributes": {"is_quickstart": 0, "source": "native_app"}
+    }
+    return sp_session
